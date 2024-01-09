d3f72bf366651a8848d2985fda8497b15899d715
Helene <helene.quintard@lapiscine.pro>
2024-01-09 06:27:38 +0100

    WIP

--- src/page/admin/AdminRequestsPage.jsx
+++ src/page/admin/AdminRequestsPage.jsx
@@ -34,7 +34,7 @@ const AdminRequestsPage = () => {
   
       const requestsResponse = await fetch("http://localhost:3005/api/requests");
         const requestsResponseData = await requestsResponse.json();
-        setRequests(requestsResponseData);
+        setRequests(requestsResponseData.data);
     };
     console.log(requests)
     return (
@@ -58,11 +58,29 @@ const AdminRequestsPage = () => {
                       <tbody>
                           {requests.map((request) => (
                               <tr key={request.id}>
-                                  <td>{request.user}</td>
+                                  <td>{request.User.firstname}</td>
+                                  <td>{request.User.lastname}</td>
                                   <td>{request.status}</td>
-                                  <td>{request.rental ? request.rental.name : "N/A"}</td>
-                                  <td>{request.holidaysVouchers ? request.holidaysVouchers.name : "N/A"}</td>
-                                  <td>{request.leisure ? request.leisure.name : "N/A"}</td>
+                                  <td>
+                                    {request.Rental && request.Rental.name ? request.Rental.name : ""}
+                                    {request.HolidaysVoucher && request.HolidaysVoucher.name ? request.HolidaysVoucher.name : ""}
+                                    {request.Leisure && request.Leisure.name ? request.Leisure.name : ""}
+                                  </td>
+                                  <td>
+                                    {request.Rental && request.Rental.amount ? request.Rental.amount : ""}
+                                    {request.HolidaysVoucher && request.HolidaysVoucher.amount ? request.HolidaysVoucher.amount : ""}
+                                    {request.Leisure && request.Leisure.amount ? request.Leisure.amount : ""}
+                                  </td>
+                                  <td>
+                                    {request.Rental && request.Rental.paymentMethod ? `${request.Rental.paymentMethod} fois` : ""} 
+                                    {request.HolidaysVoucher && request.HolidaysVoucher.paymentMethod ? `${request.HolidaysVoucher.paymentMethod} fois` : ""}
+                                    {request.Leisure && request.Leisure.paymentMethod ? `${request.Leisure.paymentMethod} fois` : ""}
+                                  </td>
+                                  <td>
+                                    {request.Rental && request.Rental.checkNumber ? request.Rental.checkNumber : ""}
+                                    {request.HolidaysVoucher && request.HolidaysVoucher.checkNumber ? request.HolidaysVoucher.checkNumber : ""}
+                                    {request.Leisure && request.Leisure.checkNumber ? request.Leisure.checkNumber : ""}
+                                  </td>
                                   {decodedToken.data.role !== 2 && (
                                       <td>
                                           <button onClick={(event) => handleDeleteRequest(event, request.id)}>Supprimer</button>
